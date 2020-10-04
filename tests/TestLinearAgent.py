f9ef5e1ad331cfdd90457f313c2fe2290f2b0bf6
JalatorrS1 <joseantonioalatorre5@gmail.com>
2020-10-04 16:47:29 +0200

    .

--- tests/TestLinearAgent.py
+++ tests/TestLinearAgent.py
@@ -4,6 +4,7 @@ from lib.Environment import DeepTradingEnvironment, LinearAgent
 
 
 import datetime
+import numpy as np
 from tqdm import tqdm
 
 out_reward_window=datetime.timedelta(days=7)
@@ -13,7 +14,8 @@ meta_parameters = {"in_bars_count": 30,
                    "state_type":"in_window_out_window"}
 
 # parameters that are related to the objective/reward function construction
-objective_parameters = {"percent_commission": .001}
+objective_parameters = {"percent_commission": .001,
+                        }
 print("===Meta Parameters===")
 print(meta_parameters)
 print("===Objective Parameters===")
@@ -21,7 +23,7 @@ print(objective_parameters)
 
 
 assets_simulation_details={"asset_1":{"method":"GBM","sigma":.1,"mean":.1},
-                    "asset_2":{"method":"GBM","sigma":.2,"mean":.2}}
+                    "asset_2":{"method":"GBM","sigma":.1,"mean":-.1}}
 
 env=DeepTradingEnvironment.build_environment_from_simulated_assets(assets_simulation_details=assets_simulation_details,
                                                                      data_hash="simulation_gbm",
@@ -33,8 +35,15 @@ env=DeepTradingEnvironment.build_environment_from_simulated_assets(assets_simula
 #                                                                      meta_parameters=meta_parameters,
 #                                                                      objective_parameters=objective_parameters)
 
-linear_agent=LinearAgent(environment=env,out_reward_window_td=out_reward_window)
-for i in tqdm(range(1000)):
-    linear_agent.REINFORCE_linear_fit()
+linear_agent=LinearAgent(environment=env,out_reward_window_td=out_reward_window,
+                         reward_function="cum_return")
+
+cov=np.array([[assets_simulation_details["asset_1"]["sigma"],0],[0,assets_simulation_details["asset_2"]["sigma"]]])
+mus=np.array([assets_simulation_details["asset_1"]["mean"],assets_simulation_details["asset_2"]["mean"]])
+
+#max return all weights should go to asset with higher mean
+linear_agent.set_plot_weights(weights=np.array([0,1]))
+
+linear_agent.REINFORCE_linear_fit()
 
 
