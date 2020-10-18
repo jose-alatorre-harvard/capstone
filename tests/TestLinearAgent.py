b3129df98d3789356eea2d1f204eff9f4fbb2f5f
JalatorrS1 <joseantonioalatorre5@gmail.com>
2020-10-18 11:09:02 +0200

    keras try 1

--- tests/TestLinearAgent.py
+++ tests/TestLinearAgent.py
@@ -49,8 +49,13 @@ x=np.array(list(weights.values())).reshape(-1,1)
 p_vol=np.sqrt(np.matmul(np.matmul(x.T,cov),x))
 p_sharpe=np.matmul(x.T,mus)/p_vol
 
-deep_agent=DeepAgent(environment=env,out_reward_window_td=out_reward_window,
+deep_agent=DeepAgent(environment=env,out_reward_window_td=out_reward_window,pre_sample=True,
                          reward_function="cum_return",sample_observations=32)
+
+
+deep_agent.set_plot_weights(weights=np.array([0,1]), benchmark_G=assets_simulation_details["asset_2"]["mean"])
+deep_agent.REINFORCE_fit()
+
 
 linear_agent=LinearAgent(environment=env,out_reward_window_td=out_reward_window,
                          reward_function="cum_return",sample_observations=32)
